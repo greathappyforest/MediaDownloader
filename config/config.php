25093dcea7242420e34c284425c71ebb62cb0691
greathappyforest <122391887@qq.com>
2017-12-01 06:17:45 -0800

    Update config.php

--- config/config.php
+++ config/config.php
@@ -4,7 +4,7 @@ $CONFIG = array (
 	// Enable password restriction (boolean)
 	'security' => true,
 		
-	// MD5 hash of choosen password. Default is 'root'. (string)
+	// MD5 hash of choosen password. helloworld
 	'password' => 'fc5e038d38a57032085441e7fe7010b0',
 	
 	// Relative folder (from MediaDownloader folder) where to store downloaded files. No need for trailing slash. (string)
