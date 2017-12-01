aba60468cd9c0559f0312b580a1e4aee557378c0
greathappyforest <122391887@qq.com>
2017-12-01 06:17:09 -0800

    Update config.php

--- config/config.php
+++ config/config.php
@@ -5,16 +5,16 @@ $CONFIG = array (
 	'security' => true,
 		
 	// MD5 hash of choosen password. Default is 'root'. (string)
-	'password' => '63a9f0ea7bb98050796b649e85481845',
+	'password' => 'fc5e038d38a57032085441e7fe7010b0',
 	
 	// Relative folder (from MediaDownloader folder) where to store downloaded files. No need for trailing slash. (string)
 	'output_folder' => 'downloads',
 		
 	// Prefered post processor between avconv or ffmpeg. (string)
-	'post_processor' => 'avconv',
+	'post_processor' => 'ffmpeg',
 		
 	// Maximum simultaneous server downloads limit. Set to 0 to disable limit. (integer)
-	'max_dl' => 3
+	'max_dl' => 8
 );
 
 ?>
